8f853b66ef15df958f40839bdded4cea4de1e77a
The-beginner-2 <vovaulchenko@gmail.com>
2022-07-12 16:54:00 +0300

    implement new css styles and rewrite whole js file
    insted of using getBoundingsRect() i've used observer

--- main.js
+++ main.js
@@ -1,37 +1,23 @@
-let animItems = document.querySelectorAll('._anim-items');
+const animItems = document.querySelectorAll('.scrl-anim');
 
-if(animItems.length > 0){
-    window.addEventListener('scroll', AnimOnSroll);
-    function AnimOnSroll(){
-        for(let i = 0; i < animItems.length; i++){
-            const animItem = animItems[i];
-            const animItemHeigth = animItem.offsetHeight;
-            const animItemOffest = offset(animItem).top;
-            const animStart = 4;
-            let animItemPoint = window.innerHeight - animItemHeigth / animStart;
-
-            if(animItemHeigth > window.innerHeight){
-                animItemPoint = window.innerHeight - window.innerHeight / animStart;
-            }
-
-            if((pageYOffset > animItemOffest - animItemPoint) && pageYOffset < (animItemOffest + animItemHeigth)){
-                animItem.classList.add('_active');
-            } 
-            else if(!animItem.classList.contains('_anim-no-hide')){
-                animItem.classList.remove('_active');
-            }
-            
-        }
-    }
-    function offset(e){
-        const rect = e.getBoundingClientRect(),
-        srollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        return{
-            top : rect.top + scrollTop,
-            left: rect.left + srollLeft
-        }
-    }
+const opitons = {
+    root: document.body,
+    rootMargin: '0px',
+    threshold: 0.75,
 }
 
-AnimOnSroll();
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            console.log(1);
+            entry.target.classList.add('show');
+        }
+    })
+}, { opitons });
+
+animItems.forEach((item) => {
+    observer.observe(item);
+});
+
+console.log(animItems)
